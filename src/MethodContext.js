8198748f2a1dd3983ead1e8a6911041d03d7f51b
Anastasia Kroshkina <nastikak@yandex.ru>
2025-04-13 20:26:53 +0300

    Задание по method-contex

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -8,8 +8,17 @@ import { EventEmitter } from './EventEmitter';
  */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    subscribe() {
+        this.clickHandler = () => {
+            this.count += 1;
+        };
+        EventEmitter.on('click', this.clickHandler);
+    },
+    unsubscribe() {
+        if (this.clickHandler) {
+            EventEmitter.off('click', this.clickHandler);
+        }
+    },
 };
 
 /*
@@ -19,7 +28,9 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        this.second(...args.reverse());
+    },
     second() {
         // здесь ничего писать не нужно
     },
